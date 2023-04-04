7baf26723f40618ca0c8d4332afdf4b33e2b5238
Leonardo1924 <leonardo1924@protonmail.com>
2023-04-04 13:34:37 +0100

    Ended Guião 2

--- Prática2/ficha2.c
+++ Prática2/ficha2.c
@@ -7,6 +7,8 @@
 
 #define MAX3 10
 #define MAX4 10
+#define LINHAS 10
+#define COLUNAS 50
 
 void exercise1(){
     pid_t child = fork();
@@ -74,6 +76,79 @@ void exercise4(){
     }
 }
 
+int exercicio5(){
+    int status, find = 32;
+    int matriz[LINHAS][COLUNAS];
+    pid_t pid;
+
+    for(int i = 0; i < LINHAS;i++){
+        if(fork() == 0){
+            for(int j = 0; j < COLUNAS; j++){
+                matriz[i][j] = rand() % 100;
+                if(matriz[i][j] == find){
+                    _exit(i);
+                }
+            }
+            _exit(-1);
+        }
+    }
+
+    for(int i = 0; i < LINHAS; i++){
+        pid = wait(&status);
+        if(WIFEXITED(status)){
+            if(WEXITSTATUS(status) != -1){
+                printf("O filho %d encontrou o número %d na linha %d\n", pid, find, WEXITSTATUS(status));
+            }
+        }
+    }
+    return 0;
+}
+
+int exercicio6(){
+    int status, find = 32;
+    int matriz[LINHAS][COLUNAS];
+    int matches[LINHAS] = {0};
+    pid_t pid;
+
+    for(int i = 0; i < LINHAS;i++){
+        if(fork() == 0){
+            for(int j = 0; j < COLUNAS; j++){
+                matriz[i][j] = rand() % 100;
+                if(matriz[i][j] == find){
+                    _exit(i);
+                }
+            }
+            _exit(-1);
+        }
+    }
+
+    for(int i = 0; i < LINHAS;i++){
+        pid = wait(&status);
+        int linha = WEXITSTATUS(status);
+        if(linha != 255)
+            matches[linha] = 1;
+    }
+
+    for(int i = 0; i < LINHAS; i++){
+        for (int j = 0; j < COLUNAS; j++)
+        {
+            printf("%2d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+
+    for(int i = 0; i < LINHAS; i++){
+        if(matches[i] == 1){
+            printf("Linha %d contém o número %d\n", i, find);
+        }
+    }
+
+    return 0;
+
+}
+
 
 int main(int argc, char *argv[]){
     char str1[] = "exe1";
@@ -91,6 +166,10 @@ int main(int argc, char *argv[]){
     char str4[] = "exe4";
     if(strcmp(argv[1], str4) == 0){
         exercise4();
+    }
+    char str5[] = "exe5";
+    if(strcmp(argv[1], str5) == 0){
+        exercicio5();
     }
     
     return 0;
